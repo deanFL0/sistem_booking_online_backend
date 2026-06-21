d3235384c3c669b94459a79308e2c6b2ffd8cccb
G <deanfarhan46@gmail.com>
2026-06-21 15:51:27 +0700

    add register request for register endpoint

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -45,7 +45,7 @@ class AuthController extends Controller
         return response()->json(['message' => 'Logged out successfully']);
     }
 
-    public function register(StoreUserRequest $request)
+    public function register(RegisterRequest $request)
     {
         $validated = $request->validated();
 
